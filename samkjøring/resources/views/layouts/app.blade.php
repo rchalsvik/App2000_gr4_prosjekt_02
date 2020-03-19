d399e534b985ead63c73c5181e5f4443f898d362
Ross Chr. Halsvik <47125013+Jaevel@users.noreply.github.com>
2020-03-19 14:14:34 +0100

    css

--- samkjøring/resources/views/layouts/app.blade.php
+++ samkjøring/resources/views/layouts/app.blade.php
@@ -32,14 +32,14 @@
       <!-- Navigation -->
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container">
-          <a class="navbar-brand" href="/test">Samkjøring AS</a>
+          <a class="navbar-brand" href="{{ route('index') }}">Samkjøring AS</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
               <li class="nav-item active">
-                <a class="nav-link" href="/test">Hjem
+                <a class="nav-link" href="{{ route('index') }}">Hjem
                   <span class="sr-only">(current)</span>
                 </a>
               </li>
@@ -71,7 +71,7 @@
     <!-- Footer -->
     <footer class="py-5 bg-dark">
       <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2020</p>
+        <p class="m-0 text-center text-white">Copyright &copy; Grp04 2020</p>
       </div>
       <!-- /.container -->
     </footer>
