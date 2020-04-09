e3da083702256e2f1b30a416d90d9645507c98fe
joachimhagheim <47362824+joachimhagheim@users.noreply.github.com>
2020-04-09 14:01:03 +0200

    Update TripController.php

--- samkjøring/app/Http/Controllers/TripController.php
+++ samkjøring/app/Http/Controllers/TripController.php
@@ -104,7 +104,7 @@ class TripController extends Controller
         Log::channel('samkjøring')->info($logString);
 
         $trip->update($this->validateTrip());
-        return redirect('trips.seeMore', ['trip' => $trip]); // Dette er hvor du blir sendt etter å ha postet!
+        return view('trips.seeMore', ['trip' => $trip]); // Dette er hvor du blir sendt etter å ha postet!
     }
 
     /**
