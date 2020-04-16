6e64587907e80a673976aa2ec4732fb8e20e41cd
Ross Chr. Halsvik <47125013+Jaevel@users.noreply.github.com>
2020-04-16 23:11:02 +0200

    enda mer html

--- samkjoring/resources/views/about.blade.php
+++ samkjoring/resources/views/about.blade.php
@@ -2,26 +2,18 @@
 
 @section('content')
 
-  <!-- Page Content -->
-  <div class="container">
 
-    <!-- Jumbotron Header -->
+  <div class="container">
     <div class="page card my-4">
-      <div class="">
-        <img id="tandem" src="img/tandem.jpg" alt="tandemsykkel">
+      <img id="tandem" src="img/tandem.jpg" alt="tandemsykkel">
+
+      <div class="page-text-area">
         <h1 class="display-3">{{ __('aboutTitle') }}</h1>
         <p class="lead">{{ __('aboutText') }}</p>
         <!-- <a href="/registration" class="btn btn-primary btn-lg">Registrer deg nå!</a> -->
       </div>
 
+
+
     </div>
-    <div id="omossbilder">
-  		<h1 id="vårebilder"></h1>
-  		<figure>
-  			<img id="tandem" src="img/tandem.jpg" alt="tandemsykkel">
-  			<figcaption><b id="tandem">{{ __('Tandem-bike') }}</b></figcaption>
-  		</figure>
-
-  	</div>
-
 @endsection
